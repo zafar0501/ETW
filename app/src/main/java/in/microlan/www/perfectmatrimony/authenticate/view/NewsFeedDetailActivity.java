8a64b52fd1f2b3ecca7e2310ca5c393f7cd5bc8e
zafarhussain <zafarhussain910@gmail.com>
2017-11-06 12:15:56 +0530

    Shefali Branch PullRequest Merge with Current Dev Branch by Zafar Hussain

---
+++ app/src/main/java/in/microlan/www/perfectmatrimony/authenticate/view/NewsFeedDetailActivity.java
@@ -0,0 +1,26 @@
+package in.microlan.www.perfectmatrimony.authenticate.view;
+
+import android.view.Menu;
+
+import in.microlan.www.perfectmatrimony.common.base.BaseActivity;
+
+/**
+ * Created by Shefali on 03/11/17.
+ */
+
+public class NewsFeedDetailActivity extends BaseActivity{
+    @Override
+    public void InitView() {
+
+    }
+
+    @Override
+    protected int getLayoutResourceId() {
+        return 0;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return false;
+    }
+}
